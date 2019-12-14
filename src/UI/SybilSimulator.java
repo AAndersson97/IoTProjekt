93a18a927d8df997da217535fca52f7745d2bcc1
AAndersson97 <33671318+AAndersson97@users.noreply.github.com>
2019-12-14 10:31:39 +0100

    *

--- src/UI/SybilSimulator.java
+++ src/UI/SybilSimulator.java
@@ -1,13 +1,18 @@
 package UI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import network.Constants;
 
 public class SybilSimulator extends Application implements Constants {
+
+    @FXML
+    public Button createNode;
 
     public static void main(String[] args){
         launch(args);
