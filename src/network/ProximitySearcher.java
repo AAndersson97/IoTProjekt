d903bb987143e81902a9ddc7d43f812ddd9d9dc3
MLindgren94 <58726944+MLindgren94@users.noreply.github.com>
2019-12-14 16:49:32 +0100

    Scene

--- src/network/ProximitySearcher.java
+++ src/network/ProximitySearcher.java
@@ -5,10 +5,35 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class ProximitySearcher {
+    private static ProximitySearcher instance;
 
-    public void findNeighbours(LocationCreator.Location location) {
-        //ArrayList<Node> nodes = NodeList.getInstance().getNodeList();
+    static {
+        instance = new ProximitySearcher();
+    }
+
+    ProximitySearcher() {
+    }
+    public static ProximitySearcher getInstance() {
+        return instance;
+    }
+
+    public ArrayList<Node> findClosestNeighbours(Node n) {
+        ArrayList<Node>neighbours = new ArrayList<>();
+        int nx = n.getLocation().getX();
+        int ny = n.getLocation().getY();
+        for(Node node : NodeList.getInstance().getNodeList().values()){
+            int nox = node.getLocation().getX();
+            int noy = node.getLocation().getY();
+            if(nox != nx && noy!=ny){
+                if(Math.abs((nox-nx))<90  && Math.abs(noy-ny)<90){
+                    neighbours.add(node);
+                }
+            }
+
+        }
+        //ArrayList<Node> nodes = (ArrayList<Node>) NodeList.getInstance().getNodeList().values();
         //Collections.sort(nodes);
-
+        //nodes.sort((n1,n2)-> Math.abs((n1.getLocation().getY() - n2.getLocation().getX()) + (n1.getLocation().getY() - n2.getLocation().getY())));
+        return neighbours;
     }
 }
