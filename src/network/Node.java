86e5372d8c250f3cd268904cee9602d703c19fac
AAndersson97 <33671318+AAndersson97@users.noreply.github.com>
2019-12-14 12:45:37 +0100

    *

--- src/network/Node.java
+++ src/network/Node.java
@@ -3,13 +3,13 @@ package network;
 import java.util.Comparator;
 
 public class Node implements Comparator<Node> {
-    private String address;
+    private Short[] address;
+    private LocationCreator.Location location;
 
     public LocationCreator.Location getLocation() {
         return location;
     }
 
-    private LocationCreator.Location location;
     public void createNode() {
 
 
@@ -17,10 +17,15 @@ public class Node implements Comparator<Node> {
 
     public Node() {
         location = LocationCreator.getInstance().createLocation();
+        address = AddressGenerator.getInstance().generateAddress();
     }
 
     @Override
     public int compare(Node o1, Node o2) {
         return Math.abs((o1.location.getY() - o2.location.getX()) + (o1.location.getY() - o2.location.getY()));
+    }
+
+    public Short[] getAddress() {
+        return address;
     }
 }
