4785658f19879e44723b1a2ca87016a570a44ca8
MLindgren94 <58726944+MLindgren94@users.noreply.github.com>
2019-12-14 14:58:29 +0100

    Scene

--- src/network/LocationCreator.java
+++ src/network/LocationCreator.java
@@ -21,12 +21,12 @@ public class LocationCreator implements Constants {
      * @return Ett Location-objekt innehållandes x- och y-koordinater.
      */
     private void createLocations() {
-        for (int y = CIRCLE_RADIUS*5, x = 10; x <  WINDOW_WIDTH; y+=CIRCLE_RADIUS*2) {
+        for (int y = CIRCLE_RADIUS*5, x = 40; x <  WINDOW_WIDTH; y+=CIRCLE_RADIUS*2) {
             locations.add(new Location(x, y));
-            y += CIRCLE_RADIUS*2;
-            if (y > WINDOW_HEIGHT-(CIRCLE_RADIUS*4)) {
-                y = (CIRCLE_RADIUS*5) + (int)(Math.random()*20);
-                x += CIRCLE_RADIUS*5;
+            y += CIRCLE_RADIUS*1;
+            if (y > WINDOW_HEIGHT-(CIRCLE_RADIUS*6)) {
+                y = (CIRCLE_RADIUS*4) + (int)(Math.random()*20);
+                x += CIRCLE_RADIUS*4;
             }
         }
     }
