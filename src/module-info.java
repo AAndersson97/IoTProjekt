778cfe2fa33511ea63eb748bfb6d3732afc85e5d
AAndersson97 <33671318+AAndersson97@users.noreply.github.com>
2019-12-19 12:30:16 +0100

    *

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,6 @@
 module IoTProjekt {
     requires javafx.fxml;
     requires javafx.controls;
-    requires rocksaw;
 
     opens UI;
-    opens network;
 }
--- src/network/OSPFServer.java
+++ src/network/OSPFServer.java
@@ -1,12 +1,9 @@
 package network;
 
-import com.savarese.rocksaw.net.RawSocket;
-
 import java.io.IOException;
 import java.net.InetAddress;
 
 public class OSPFServer implements Constants {
-    private RawSocket socket;
     private boolean multicast;
     public OSPFServer(InetAddress address) {
         multicast = false;
@@ -14,22 +11,7 @@ public class OSPFServer implements Constants {
     }
 
     private void configureSocket(InetAddress address) {
-        socket = new RawSocket();
 
-        try {
-            socket.open(RawSocket.PF_INET, 89);
-            socket.setUseSelectTimeout(true);
-            socket.setReceiveTimeout(TIME_OUT);
-            socket.setSendTimeout(TIME_OUT);
-            if (multicast) {
-                socket.setIPHeaderInclude(false);
-            } else {
-                socket.bind(address);
-            }
-            System.out.println("Socket Opened");
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
     }
 
 }
