0934b2e58f2c0ea50526cba526e9bff088c45a3a
MLindgren94 <58726944+MLindgren94@users.noreply.github.com>
2020-01-06 15:16:17 +0100

    Scene

--- src/UI/SybilSimulator.java
+++ src/UI/SybilSimulator.java
@@ -1,5 +1,6 @@
 package UI;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -22,13 +23,16 @@ import java.util.Arrays;
 
 public class SybilSimulator extends Application {
 
-
+    @FXML
+    Slider slider;
     @FXML
     MenuBar menuDisplay;
     @FXML
     MenuItem TAMenu;
     @FXML
     MenuItem IPMenu;
+    @FXML
+    MenuItem animate;
     @FXML
     AnchorPane anchorPane;
     @FXML
@@ -41,6 +45,7 @@ public class SybilSimulator extends Application {
     private static ArrayList<Circle> taCircles;
     private static boolean TAVisible; // true om överföringsareor är synliga
     private static boolean IPVisible;
+    private static int packetTransportDelay;
 
     private static ArrayList<Label> addressLabels = new ArrayList<>();
 
@@ -90,7 +95,7 @@ public class SybilSimulator extends Application {
     private Label createAddresslabel(Node node) {
         String address = Arrays.toString(node.getAddress()).replace(", ", ".");
         Label nodeLabel = new Label(address.substring(1, address.length()-1));
-        nodeLabel.relocate(node.getLocation().getX()-29,node.getLocation().getY()+12);
+        nodeLabel.relocate(node.getLocation().getX()-23,node.getLocation().getY()+12);
         nodeLabel.setVisible(IPVisible);
         return nodeLabel;
     }
@@ -177,5 +182,30 @@ public class SybilSimulator extends Application {
             transition.play();
         });
 
+    }
+
+    public void animateDroppedPath(){
+        Circle newCircle = new Circle(2, Color.RED);
+        ArrayList<Node> test = Network.getNodeList();
+        anchorPane.getChildren().add(newCircle);
+        Line newLine = new Line();
+        newLine.setStartX(test.get(0).getLocation().getX());
+        newLine.setStartY(test.get(0).getLocation().getY());
+        newLine.setEndX(test.get(0).getLocation().getX());
+        newLine.setEndY(test.get(0).getLocation().getY()+20);
+        PathTransition transition = new PathTransition();
+        transition.setNode(newCircle);
+        transition.setDuration(Duration.millis(500));
+        transition.setPath(newLine);
+        transition.setCycleCount(1);
+        FadeTransition fadeTransition =
+                new FadeTransition(Duration.millis(1000), newCircle);
+        fadeTransition.setFromValue(1.0f);
+        fadeTransition.setToValue(0.0f);
+        transition.play();
+        fadeTransition.play();
+    }
+    public void sliderDragged(){
+        System.out.println((int)slider.getValue());
     }
 }
